c258480870f3bf374d91515d7cdf0f2c1b57ee08
p1282 <49135456+p1282@users.noreply.github.com>
2026-07-14 21:15:56 +0900

    Update script.js

--- script.js
+++ script.js
@@ -31,8 +31,8 @@ document.addEventListener('DOMContentLoaded', () => {
         const username = document.getElementById('username').value;
         const password = document.getElementById('password').value;
 
-        // 💡 아이디와 비밀번호 확인 (예시: 'user' / '1234')
-        if (username === 'user' && password === '1234') {
+        // 💡 아이디와 비밀번호 확인 (예시: 'user' / '1111')
+        if (username === 'user' && password === '1111') {
             alert('로그인 성공!');
             loginModal.style.display = 'none';
             // 메뉴 활성화
